7469a2d7f95672d289bbb053dd0ef90f6368143b
nasiima <nasiima.mohamed1@gmail.com>
2022-06-06 12:26:10 +0100

    adding routes

---
+++ routes/habits.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const {
+    getHabits,
+    // completEHabit,
+    createHabit,
+    updateHabit,
+    deleteHabit
+} = require('../controllers/habits')
+const router = express.Router();
+
+router.route('/')
+.get(getHabits)
+.post(createHabit)
+
+router.route('/:id')
+.put(updateHabit)
+// .patch(completeHabit)
+.delete(deleteHabit)
+
+
+
+module.exports = router; 
