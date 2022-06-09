89d5be9945abc7e2e0e57f51713588deb582ae7e
Shoreen Beckford <shoreen.beckford@gmail.com>
2022-06-09 14:11:01 +0100

    Amend test for habits schema

--- test/habits.test.js
+++ test/habits.test.js
@@ -1,5 +1,59 @@
 const mockingoose = require('mockingoose');
-const BooksModel = require('../models/habits');
-const {
-  fetchHabits
-} = require('./habits');
+const habitsModel = require('../models/habits');
+const {getHabit, createHabit, updateHabit} = require('../controllers/habits');
+
+describe('Habits service', () => {
+  describe('getHabit', () => {
+    it ('should return the list of habits', async () => {
+      mockingoose(habitsModel).toReturn([
+        {
+          username: 'testname',
+          
+          habits: {
+            water: {
+              max: 6,
+              current: 2,
+              active: false,
+            },
+            outdoors: {
+              max: 0,
+              current: 0,
+              active: false,
+            },
+            code: {
+              max: 0,
+              current: 0,
+              active: false,
+            },
+            projects: {
+              max: 0,
+              current: 0,
+              active: false,
+            },
+          },
+        
+          streaks: {
+            water: {
+              max: 0,
+              current: 0,
+            },
+            outdoors: {
+              max: 0,
+              current: 0,
+            },
+            coding: {
+              max: 0,
+              current: 0,
+            },
+            projects: {
+              max: 0,
+              current: 0,
+            },
+          },
+        }
+      ], 'find');
+      const results = await getHabit();
+      expect(results[0].username).toBe('testname');
+    });
+  });
+});
